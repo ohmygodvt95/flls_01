36dd3a0eaf426f22581996940eb0a0af785cb1d3
Le Tat Tung <letattungtb@gmail.com>
2017-03-10 15:42:10 +0700

    static pages

--- db/migrate/20170309044656_create_relationships.rb
+++ db/migrate/20170309044656_create_relationships.rb
@@ -1,7 +1,7 @@
 class CreateRelationships < ActiveRecord::Migration[5.0]
   def change
-    create_table :follows do |t|
-      t.integer :follwer_id
+    create_table :relationships do |t|
+      t.integer :follower_id
       t.integer :followed_id
       t.datetime :created_at
       t.datetime :updated_at
